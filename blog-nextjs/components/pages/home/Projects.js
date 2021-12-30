b3cfcd61e3941c7974618177fef8590dcdf4864b
Ariane <ariane.ernandorena@gmail.com>
2021-12-30 17:25:31 -0500

    Added projects h2

--- blog-nextjs/components/pages/home/Projects.js
+++ blog-nextjs/components/pages/home/Projects.js
@@ -1,12 +1,13 @@
-import { useEffect  } from "react";
-import Image from "next/image";
+
+import Link from "next/link";
+import useMediaQuery from "../../../hooks/useMediaQuery";
+import styles from "../../../styles/pages/Home.module.scss";
+
+// Assets
 import WaveA from "../../../public/images/wave-dividers/purple-wave-a.svg";
 import WaveB from "../../../public/images/wave-dividers/purple-wave-b.svg";
 import WaveC from "../../../public/images/wave-dividers/purple-wave-c.svg";
-
-import useMediaQuery from "../../../hooks/useMediaQuery";
-
-import styles from "../../../styles/pages/Home.module.scss";
+import { FaCircle } from "react-icons/fa";
 
 const Projects = () => {
 
@@ -16,10 +17,21 @@ const Projects = () => {
         <>
         <div className={styles["wave-divider"]}>
             { isAtLeastMd ? <WaveA/> : <WaveB/>}
-            {/* <WaveC/> */}
         </div>
+
         <section className={styles["projects-wrapper"]}>
-            Projects here
+            <div className={styles["inner"]}>
+
+                <div className={styles["section-title-wrapper"]}>
+                    <div className={styles["dots"]}><FaCircle/></div>
+                    <Link href="/projects">
+                        <a><h2>Projects</h2></a>
+                    </Link>
+                    <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas vitae lectus lectus. Donec sit amet nulla eget nunc egestas dapibus. Suspendisse potenti.
+                    </p>
+                </div>                
+            </div>    
         </section>
         </>
         
